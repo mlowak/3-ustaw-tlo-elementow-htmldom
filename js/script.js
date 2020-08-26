232bf90d38c3c7be4bf2a9ac0fab021fd64a15b1
mlowak <67906604+mlowak@users.noreply.github.com>
2020-08-26 18:20:29 +0200

    dodanie bg w css

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
 // ES6
 
 const setBackground = () => {
-    console.log('click');
+    let pl = document.querySelector('.first');
+    let p2 = document.querySelector('.last');
+
+    // nawiązuje do CSS
+    // p1.classList.add('bg-red');
+    // p2.classList.add('bg-yellow');
+
+    // toggle sprawia, że class pojawia się lub znika (w tej sytuacji przy naciśnięciu na btn)
+    p1.classList.toggle('bg-red');
+    p2.classList.toggle('bg-yellow');
+    
+
+    // p1.style.backgroundColor = 'red';
+    // p2.style.backgroundColor = 'yellow';
+    // zmiana style inline, nie rekomendowane
 }
 
 let btnSetBackground = document.getElementById('set-bg');
