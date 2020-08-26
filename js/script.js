8869c8e3bc9c176aacb513ae3b2f8a73216c0d52
mlowak <67906604+mlowak@users.noreply.github.com>
2020-08-26 17:48:14 +0200

    dodanie btn

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// ES6
+
+const setBackground = () => {
+    console.log('click');
+}
+
+let btnSetBackground = document.getElementById('set-bg');
+
+btnSetBackground.addEventListener('click', setBackground);
